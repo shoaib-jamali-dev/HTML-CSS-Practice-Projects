81279cf748dc2af03c889613ad2d99194958c9a6
shoaib-jamali-dev <shoaibjamali30@gmail.com>
2026-04-21 15:41:56 +0500

    scroll progress bar

---
+++ scroll_progress_bar/script.js
@@ -0,0 +1,12 @@
+let indicator = document.querySelector(".progress")
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+window.addEventListener("scroll",scroll)
+
+function scroll(){
+    let scrolltop = document.documentElement.scrollTop
+    let scrolled = (scrolltop/scrollHeight)*100
+    indicator.style.width = `${scrolled}%`
+    console.log(indicator.style.width = `${scrolled}%`);
+    
+}
